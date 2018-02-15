5d073e73a3b766dc419b85a9127229e496aef86d
J. Moilanen <jnsmoilanen@gmail.com>
2018-02-15 20:48:38 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sum = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.reduce(sum, 0)
+}
+
 module.exports = {
   average,
-  dummy
+  dummy,
+  totalLikes
 }
