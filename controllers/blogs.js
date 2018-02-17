efb237a6eafb96783150b3f26a6f981f654ffb52
J. Moilanen <jnsmoilanen@gmail.com>
2018-02-17 17:58:27 +0200

    4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -13,17 +14,32 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map(Blog.format))
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+
   try {
-    const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
 
     if (body.title === undefined || body.url === undefined) {
       return response.status(400).json({ error: 'title and/or url missing' })
     }
 
-    const user = await User.findById(body.userId)
+    const user = await User.findById(decodedToken.id)
     if(user === undefined) {
-      return response.status(400).json({error: 'useria ei löytynyt'})
+      return response.status(400).json({error: 'no user'})
     }
 
     const blog = new Blog({
@@ -42,8 +58,12 @@ blogsRouter.post('/', async (request, response) => {
 
     response.status(201).json(Blog.format(savedBlog))
   } catch (exception) {
-    console.log(exception)
-    response.status(500).json({ error: 'problems with POST...' })
+    if (exception.name === 'JsonWebTokenError' ) {
+      response.status(401).json({ error: exception.message })
+    } else {
+      console.log(exception)
+      response.status(500).json({ error: 'problems with POST...' })
+    }
   }
 })
 
